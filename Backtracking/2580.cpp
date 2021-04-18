72305a65aba66e22e3f537ca3509dcc03eb542b9
yongbeomkwak <yongbeomkwak@yongbeomkwakui-MacBookPro.local>
2021-04-17 19:18:57 -0700

    solve 2580

--- Backtracking/2580.cpp
+++ Backtracking/2580.cpp
@@ -1,127 +1,119 @@
 #include <iostream>
-#include <cstring>
+#include <algorithm>
 #include <vector>
-#include <sstream>   //istringstream 사용 (ㅎ)
-#include <algorithm> // std::find
 using namespace std;
-int in[9][9] = {0};
 
-void print() //결과 출력
+int sudoku_arr[9][9] = {0};
+
+// 수도쿠 값을 출력해준다.
+void print_sudoku()
 {
     for (int i = 0; i < 9; i++)
     {
         for (int j = 0; j < 9; j++)
         {
-            printf("%d ", in[i][j]);
+            cout << sudoku_arr[i][j];
+            if (j != 8)
+                cout << " ";
         }
-        printf("\n");
+        cout << endl;
     }
 }
-void insert(int row, int col)
+
+void sudoku_slove(int row, int col)
 {
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9}; //각 검사에서 나온 숫자들을 벡터에서 모두 제거한다
+    // 위치에서 가능한 값을 체크한다. (가능한 값을 배열에 넣자)
+    int used_num_arr[9] = {0};
+    vector<int> each_round_can_vec;
 
-    if (in[row][col] != 0)
+    // 가로 세로 체크
+    for (int i = 0; i < 9; i++)
     {
-        return;
+        if (sudoku_arr[row][i])                       //가로 체크
+            used_num_arr[sudoku_arr[row][i] - 1] = 1; //인덱스는 0 , 스도쿠 값은 1부터 시작하므로  -1을 해줘 인덱스로 사용 후,사용한 인덱스 값은 1로 바꿔줌
+        if (sudoku_arr[i][col])                       //세로 체크
+            used_num_arr[sudoku_arr[i][col] - 1] = 1;
     }
-    else
+
+    // 그룹의 첫번째 시작놈을 기준으로 인덱스 설정
+    int sub_row = row / 3, sub_col = col / 3;
+    sub_row *= 3;
+    sub_col *= 3;
+
+    // 정사각형 그룹체크
+    for (int i = sub_row; i < sub_row + 3; i++)
     {
-        for (int i = 0; i < 9; i++)
+        for (int j = sub_col; j < sub_col + 3; j++)
         {
+            if (sudoku_arr[i][j])
+                used_num_arr[sudoku_arr[i][j] - 1] = 1;
+        }
+    }
 
-            if (in[row][i]) //가로 검사(0이 아닐 때)
+    // 사용되지 않은 숫자들을 전부 벡터에 넣어준다.
+    for (int i = 0; i < 9; i++)
+    {
+        if (!used_num_arr[i])                    //해당 값이 0 이면 , 즉 한번도 사용되지 않을 경우
+            each_round_can_vec.push_back(i + 1); //백터에 넣음  i=0 이란 뜻은 1을 한번도 안썼다는 뜻이니 i+1
+    }
+
+    // 사용되지 않은 숫자가 없으면 그냥 리턴하고, 있으면 해당 위치에 값을 넣고 재귀함수로 다음 단계로 간다.
+    if (!each_round_can_vec.size())
+        return;
+
+    //  다음 단계로 넘어간다.
+    int next_row = row, next_col = col;
+    while (true)
+    {
+        // 다음 컬럼이 9가 되면
+        if ((++next_col) == 9)
+        {
+            next_row++;
+            next_col = 0;
+            // 만약 모든 수가 끝났다면, 프린트하고 종료한다.
+            if (next_row == 9)
             {
-
-                auto it = find(v.begin(), v.begin() + v.size(), in[row][i]); //find(시작,끝,찾을 값) ->찾을 시 해당 위치주소,없을 시 end 주소
-                if (it != v.end())                                           //end가 아니면  찾았다는 것
-                {
-                    v.erase(it); //지움
-                }
+                sudoku_arr[row][col] = each_round_can_vec[0]; //마지막은 벡터에 하나만 있으므로 검사가 끝난 벡터에서 첫번째 값을 바로 넣음
+                print_sudoku();
+                exit(0);
             }
         }
-        for (int i = 0; i < 9; i++)
-        {
-            if (in[i][col]) //세로 검사(0이 아닐 때)
-            {
-                auto it = find(v.begin(), v.begin() + v.size(), in[i][col]); //find(시작,끝,찾을 값) ->찾을 시 해당 위치주소,없을 시 end 주소
-                if (it != v.end())                                           //end가 아니면  찾았다는 것
-                {
-                    v.erase(it); //지움
-                }
-            }
-        }
-        int sub_row = 0;
-        int sub_col = 0;
-        sub_row = row / 3;
-        sub_col = col / 3;
-        sub_row *= 3;
-        sub_col *= 3;
-        // 정사각형 구역 검사를 하기위해서 해당 행,열 정보를 이용하여 sub 행,열을 만난다
-        // / 연산자를 이용하여 나머지를 버리고 몫만을 얻은 후 (*3) 3을 곱하여 해당 행,열이 포함된 정사각형의 위치로 매핑된다
+        if (next_row == 9)
+            break;
+        if (sudoku_arr[next_row][next_col] == 0) //다음 값이 0이면 다시 검사를  해야 하므로 while 벗어남
+            break;
+    }
 
-        //정사각형 구역 검사
-        for (int i = sub_row; i < sub_row + 3; i++) //정사각형 가로
-        {
-            for (int j = sub_col; j < sub_col + 3; j++) //정사각형 세로
-            {
-                if (in[i][j]) //(0이 아닐 때)
-                {
-                    auto it3 = find(v.begin(), v.begin() + v.size(), in[i][j]);
-                    if (it3 != v.end())
-                    {
-                        v.erase(it3); //마찬가지로 지움
-                    }
-                }
-            }
-        }
-
-        if (v.size() == 1)
-        {
-            in[row][col] = v[0]; //모두 제거하고 남은 값(즉,1~9 중 하나도 나오지 않았던 값)
-        }
+    for (int i = 0; i < each_round_can_vec.size(); i++)
+    {
+        //만약 가능성이 있는 값이 1 , 5 ,7,8 이면 하나씩 넣은 후 다음 스텝으로 진행함,
+        // 만약 리턴하여 빠져나올 경우, 다음 경우인 5로 다시 탐색 들어감
+        sudoku_arr[row][col] = each_round_can_vec[i]; // 검사에서 남은 값들을 차례대로 넣고
+        sudoku_slove(next_row, next_col);             //다음 단계로 넘어감
+        sudoku_arr[row][col] = 0;                     //이후 다시 값을 돌려놓아서 다음 값으로 확인해봄
     }
 }
 
-void bfs(const int &row, const int &col)
-{
-    int next_row = row;
-    int next_col = col + 1;
-    if (row == 9) // 행을 벗어나면 멈춤
-    {
-        return;
-    }
-    if (col == 9) // 열을 벗어나면 다음 열로 이동
-    {
-        next_col = 0;
-        next_row += 1;
-    }
-    if (!in[row][col]) //값이 0일 경우 검사 필요 insert 실행
-    {
-        insert(row, col);
-    }
-    bfs(next_row, next_col); // 다음 스텝으로 이동
-}
 int main()
 {
-
     for (int i = 0; i < 9; i++)
     {
-        string a;
-        getline(cin, a);     //한줄 입력받음(cin,string object)
-        istringstream ss(a); // instringstream 변수(해당 string object) 공백과 줄바꿈 무시
-
-        //만약 a가 1 2 3 4 5 6 7 8 9
-        //ss는 123456789
-        //ss>>  할 시 1 부터 하나씩 빼서 넣음
         for (int j = 0; j < 9; j++)
         {
-            int val;
-            ss >> val; //한 줄 입력받은 것에서 공백 기준으로 하나씩 들어감
-            in[i][j] = val;
+            cin >> sudoku_arr[i][j];
         }
     }
 
-    bfs(0, 0);
-    print();
+    // 그룹 체크
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (sudoku_arr[i][j] == 0)
+            {
+                sudoku_slove(i, j);
+            }
+        }
+    }
+    return 0;
 }
