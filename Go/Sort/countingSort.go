74688ec2f5c47144b44c460450b306951c396927
yongbeomkwak <yongbeomkwak@yongbeomkwakui-MacBookPro.local>
2021-05-09 22:32:43 +0900

    Add counting Sort..

--- Go/Sort/countingSort.go
+++ Go/Sort/countingSort.go
@@ -2,27 +2,39 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
+
+func countingSort(raw_list []int, max_val int) []int {
+
+	result := make([]int, max_val+1)
+
+	for _, val := range raw_list {
+		result[val]++
+	}
+	return result
+}
 
 func main() {
 	//reader := bufio.NewReader(os.Stdin)
 	num := 0
 	fmt.Scan(&num)
 	in_list := make([]int, 0)
+	max_val := 0
 	for i := 0; i < num; i++ {
 		temp := 0
 		fmt.Scan(&temp)
+		if max_val < temp {
+			max_val = temp
+		}
 		in_list = append(in_list, temp)
 	}
-	sort.Slice(in_list, func(i, j int) bool {
-		if in_list[i] < in_list[j] {
-			return true
-		}
-		return false
-	})
 
-	for _, val := range in_list {
-		fmt.Println(val)
+	result := countingSort(in_list, max_val)
+	for idx, val := range result {
+		if val != 0 {
+			for i := 0; i < val; i++ {
+				fmt.Println(idx)
+			}
+		}
 	}
 }
