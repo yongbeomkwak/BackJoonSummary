fbd6781792287ce3fa9da3ac83fb16ae2b8163dd
yongbeomkwak <yongbeomkwak@yongbeomkwakui-MacBookPro.local>
2021-06-14 20:49:37 +0900

    Add Queue step

--- C++/Queue/18258.cpp
+++ C++/Queue/18258.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+
 typedef unsigned long long ull;
 using namespace std;
 
@@ -14,82 +15,107 @@ typedef struct _Node
 //name convention을 맞춰주면 좋습니다.
 //보통 struct는 대문자, 변수들은 소문자, 함수도 소문자로 합니다.
 //꼭 이 convention을 맞출 필요는 없지만 본인만의 naming convention을 지키면 좋습니다.
-typedef struct _Stack
+typedef struct _Queue
 {
-    Pnode peak;
+    Pnode front;
+    Pnode back;
     int size;
 
-} Stack, *Pstack;
+} Queue, *Pqueue;
 
 //name convention을 맞춰주면 좋습니다.
 //보통 struct는 대문자, 변수들은 소문자, 함수도 소문자로 합니다.
 //꼭 이 convention을 맞출 필요는 없지만 본인만의 naming convention을 지키면 좋습니다.
-Pstack createStack()
+Pqueue createQueue()
 {
-    return (Pstack)calloc(1, sizeof(Stack));
+    return (Pqueue)calloc(1, sizeof(Queue));
     //안에 원소들이 전부 0이면 calloc을 호출하는 편이 좋습니다.
     //OS가 메모리블록을 전부 0으로 초기화할 수 있어서 malloc호출 후 0으로 대입하는 것보다
     //빠르거나 동일한 속도(0으로 초기화된 메모리가 없는 경우)가 나옵니다.
 }
-void push(Pstack stack, int newInt)
+void push(Pqueue queue, int newInt)
 {
     Pnode newNode = (Pnode)malloc(sizeof(Node)); //C언어에서는 형변환 명시 안해주셔도 됩니다.
     newNode->data = newInt;
-    newNode->next = stack->peak;
-    stack->peak = newNode;
-    stack->size++;
+    if (queue->size == 0) // Stack부분에서 여기가 많이 바뀜
+    {
+        queue->front = newNode;
+        queue->back = newNode;
+    }
+    else
+    {
+        queue->back->next = newNode;
+        queue->back = newNode;
+    }
+    queue->size++;
     //이렇게 쓴다면 케이스를 나누지 않고 구현 가능합니다.
 }
 
-int pop(Pstack stack)
+int pop(Pqueue queue)
 {
-    if (stack->size == 0)
-        return 0;
+    if (queue->size == 0)
+        return -1;
     int data;
     Pnode tempNode;
-    data = stack->peak->data;
-    tempNode = stack->peak;
-    stack->peak = stack->peak->next;
-    stack->size--;
+    data = queue->front->data;
+    tempNode = queue->front;
+    queue->front = queue->front->next;
+    queue->size--;
     free(tempNode); //free 안해주시면 메모리 누수가 발생합니다.
     return data;
 }
-int empty(Pstack stack)
+int empty(Pqueue queue)
 {
-    return stack->size == 0 ? 1 : 0;
+    return queue->size == 0 ? 1 : 0;
 }
-int top(Pstack stack)
+int front(Pqueue queue)
 {
-    return stack->size == 0 ? -1 : stack->peak->data;
+    return queue->size == 0 ? -1 : queue->front->data;
 }
-int size(Pstack stack)
+int back(Pqueue queue)
 {
-    return stack->size;
+    return queue->size == 0 ? -1 : queue->back->data;
 }
+int size(Pqueue queue)
+{
+    return queue->size;
+}
+
 int main()
 {
     int T;
     int input;
-    Pstack Stack = createStack();
+    char str[20];
+    Pqueue queue = createQueue();
     scanf("%d", &T);
     for (int i = 0; i < T; i++)
     {
-        scanf("%d", &input);
-        if (input == 0)
+
+        scanf("%s", str);
+        if (!strcmp(str, "push"))
         {
-            pop(Stack);
+            scanf("%d", &input);
+            push(queue, input);
         }
-        else
+        else if (!strcmp(str, "pop"))
         {
-            push(Stack, input);
+            printf("%d\n", pop(queue));
+        }
+        else if (!strcmp(str, "size"))
+        {
+            printf("%d\n", size(queue));
+        }
+        else if (!strcmp(str, "empty"))
+        {
+            printf("%d\n", empty(queue));
+        }
+        else if (!strcmp(str, "front"))
+        {
+            printf("%d\n", front(queue));
+        }
+        else if (!strcmp(str, "back"))
+        {
+            printf("%d\n", back(queue));
         }
     }
-    int n = size(Stack);
-    ull sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += pop(Stack);
-    }
-    printf("%llu", sum);
-    return 0;
 }
--- C++/Stack/9012.cpp
+++ C++/Stack/9012.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
