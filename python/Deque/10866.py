1e321d878a7ad14d2aa7d30f304f8bc3e178dbae
yongbeomkwak <yongbeomkwak@yongbeomkwakui-MacBookPro.local>
2021-06-15 19:44:13 +0900

    Add queue,deque .py

---
+++ python/Deque/10866.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    q = deque()
+
+    for _ in range(N):
+        input_split = sys.stdin.readline().rstrip().split()
+
+        order = input_split[0]
+
+        if order == "push_back":
+            q.appendleft(input_split[1])
+        elif order == "push_front":
+            q.append(input_split[1])
+        elif order == "pop_front":
+            if q:
+                print(q.pop())
+            else:
+                print(-1)
+        elif order == "pop_back":
+            if q:
+                print(q.popleft())
+            else:
+                print(-1)
+        elif order == "size":
+            print(len(q))
+        elif order == "empty":
+            if q:
+                print(0)
+            else:
+                print(1)
+        elif order == "front":
+            if q:
+                print(q[-1])
+            else:
+                print(-1)
+        elif order == "back":
+            if q:
+                print(q[0])
+            else:
+                print(-1)
