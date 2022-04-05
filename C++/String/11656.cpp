8f97d865edf0d2c877556af75054300cec498027
yongbeomkwak <yongbeomkwak@gmail.com>
2022-04-05 17:41:47 +0900

    St

---
+++ C++/String/11656.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    vector<string> ret;
+    string in;
+    cin >> in;
+
+    while (!in.empty())
+    {
+        ret.push_back(in);
+        in.erase(in.begin());
+    }
+    sort(ret.begin(), ret.end());
+
+    for (const string &s : ret)
+    {
+        cout << s << endl;
+    }
+}
